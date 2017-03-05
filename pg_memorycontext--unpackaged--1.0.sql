15f9fd16673aaefd07b715f081ba7eaca49f1d29
jackgo <mutex73@gmail.com>
2017-03-05 17:59:16 +0800

    finish sql file

---
+++ pg_memorycontext--unpackaged--1.0.sql
@@ -0,0 +1,23 @@
+/* contrib/pg_memorycontext/pg_memorycontext--unpackaged--1.0.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pg_memorycontext" to load this file. \quit
+
+ALTER EXTENSION pg_memorycontext ADD function pg_memorycontext();
+ALTER EXTENSION pg_memorycontext ADD view pg_memorycontext;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
